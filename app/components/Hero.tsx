106ea8a4bc1bdbc92296373dc5cee90d02dd0cb4
shohedul1 <your-email@example.com>
2024-01-26 14:16:48 +0600

    frist-commit1

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
         <div className='px-[20px] lg:px-[280px]'>
             <h1 className='text-center text-[32px] leading-[40px] font-medium text-[#172026] lg:text-[64px] lg:leading-[72px]'>Start monitoring your website like a pro</h1>
              <p className='text-center pt-6 text-[#36485C] lg:text-[18px] lg:leading-7'>
-                Get a bird's eye view with our customizable dishboard. Stay on top of things! Revamp your work process with our game-changing feature. Boost productivity and efficiency!
+                Get a bird&apos;s eye view with our customizable dishboard. Stay on top of things! Revamp your work process with our game-changing feature. Boost productivity and efficiency!
              </p>
              <div className='flex w-full pt-8 justify-center gap-x-6'>
                 <button className='bg-[#4328EB] w-1/2 py-4 px-8 text-white rounded-[4px] lg:w-fit'>Try for free</button>
@@ -36,11 +36,11 @@ export default function Hero() {
                 <div className="flex w-full flex-col items-center lg:container lg:flex-row lg:justify-between lg:px-20">
                     <p className="text-[#FFFFFF] text-center lg:text-[18px]">Trusted by these companies</p>
                     <div className="grid grid-cols-3 items-center justify-center justify-items-center px-[20px] align-middle lg:grid-cols-5">
-                        <Image src={Google} alt=""/>
-                        <Image src={Slack} alt=""/>
-                        <Image src={Truspilot} alt=""/>
-                        <Image src={Cnn} alt=""/>
-                        <Image src={Cluth} alt=""/>
+                        <Image src={Google} alt="google"/>
+                        <Image src={Slack} alt="slack"/>
+                        <Image src={Truspilot} alt="truspilot"/>
+                        <Image src={Cnn} alt="cnn"/>
+                        <Image src={Cluth} alt="cluth"/>
 
                     </div>
                 </div>
